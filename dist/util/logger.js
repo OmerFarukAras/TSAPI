2e28179421ba055086b17c4f401173f355a4c2cf
ElixxRadeS <omerfarukaras16@gmail.com>
2023-02-18 16:15:36 +0000

    REMOVED DIST

--- dist/util/logger.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ng2_logger_1 = require("ng2-logger");
-const log = ng2_logger_1.Log.create("TSC");
-exports.default = log;
